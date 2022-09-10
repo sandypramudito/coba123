110731c8a13ac7894db4faaa4f60ca0c83dcf1b7
sandypramudito <113221720+sandypramudito@users.noreply.github.com>
2022-09-10 09:34:08 +0700

    commit pertama
    pelatihan coding

---
+++ file utama.py
@@ -0,0 +1,28 @@
+nama = "Nama saya Sandy"
+angka = 12
+angka_pecahan = 1.5
+tipe_boolean = False 
+tipe_list = ["Budi", "Tono", 12]
+tipe_set = {1,2,2,3,3,3,4,4,4,4, "abc"}
+pertambahan = 100 + 2 + 2
+pengurangan = 100 - 72
+perkalian = "he" * 3
+pembagian = 16 / 3
+pembagian_habis = 19 // 5
+modulus = 19 % 3
+
+
+print(nama)
+print("tipe int", angka)
+print("tipe float", angka_pecahan)
+print("boolean : ")
+print(tipe_boolean)
+print(tipe_list)
+print(tipe_list [1])
+print(tipe_set)
+print(pertambahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian)
+print(pembagian_habis)
+print('modulus 19 % 3 =' , modulus)
