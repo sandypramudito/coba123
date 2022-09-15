50c05b287cf5e00e0cef4345d2f15d84d68a1902
sandypramudito <113221720+sandypramudito@users.noreply.github.com>
2022-09-15 17:41:12 +0700

    Add files via upload
    Tugas Menghitung Volume Balok & Kubus

---
+++ menghitung kubus-balok.py
@@ -0,0 +1,16 @@
+print ("Menghitung Volume kubus")
+
+sisi = int(input('sisi : '))
+
+V = sisi*sisi*sisi
+
+print ("Jika sisi kubus adalah",sisi,"maka Volume kubus adalah", V )
+
+print("Menghitung Volume balok")
+p = int(input('panjang balok : '))
+l = int(input('Lebar balok : '))
+t = int(input('tinggi balok : '))
+
+v = p*l*t
+
+print ("Jika panjang balok",p,"lebar balok",l,"dan tinggi balok",t,"Volume balok adalah", v)
